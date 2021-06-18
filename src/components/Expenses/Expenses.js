36b27e67ed82708f54262187edd1d6c68a23e1cf
Tina <Tina@macusers-MBP-4.attlocal.net>
2021-06-18 14:55:00 -0500

    filtering by year functionality complete

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -18,37 +18,38 @@ function Expenses(props) {
       setFilteredYear(selectedYear)
     }
 
+    const filteredExpenses = props.items.filter(expense => {
+      return expense.date.getFullYear().toString() === filteredYear;
+    })
+
   return (
     <div>
       
         <Card className="expenses">
           
           {/* passing a function down to the ExpenseFilter component, and the child componenet will call the function when event is triggered*/}
-          <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/> 
+          <ExpenseFilter 
+            selected={filteredYear} 
+            onChangeFilter={filterChangeHandler}  
+          /> 
           {/* Controlled component - value is passed on to parent component and received from PC */}
           {/* Two way binding - selectedYear is passed down as props, and onChangeFilter is passing data up from ExpenseFilter */}
           {/* prop = onChangeFilter, pointing at ChangeHandler function */}
 
-          <ExpenseItem
+          {filteredExpenses.map((expense) => (
+            <ExpenseItem 
+              key={expense.id}
+              title={expense.title} 
+              amount={expense.amount} 
+              date={expense.date}
+            />
+          ))}
+
+          {/* <ExpenseItem
             title={props.items[0].title}
             amount={props.items[0].amount}
             date={props.items[0].date}
-          />
-          <ExpenseItem
-            title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}
-          />
-          <ExpenseItem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}
-          />
-          <ExpenseItem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}
-          />
+          /> */}
         </Card>
     </div>
   );
